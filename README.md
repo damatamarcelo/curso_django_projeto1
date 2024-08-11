83208c3819bcf31d959e83c74bc34fb19b1dacf4
Marcelo Costa da Mata <mcosta.matta@gmail.com>
2024-08-11 17:38:25 -0300

    Criação de app, arquivoss e demais ajustes.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Criação do repositório
 Criação do .gitignore
 Criação project + explicação urls.py
+Criação app recipes, arquivo urls.py em recipes. Migração de estrutura para views e urls em recipes
--- project/urls.py
+++ project/urls.py
@@ -15,25 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("HOME")
-
-
-def contato(request):
-    return HttpResponse("contato")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # HOME
-    path('sobre', sobre),  # sobre
-    path('contato', contato),  # contato
+    path('', include('recipes.urls'))
 ]
